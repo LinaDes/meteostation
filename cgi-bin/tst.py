ad00f07a56cfd7c54521fd6592586c44f6d009d3
ndrwk <ndrwkalinin@gmail.com>
2016-01-31 10:16:53 +0600

    +cgi-bin

---
+++ cgi-bin/tst.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# - *- coding: utf- 8 - *-
+
+print "Content-type: text/html"
+print
+print "<title>Test CGI</title>"
+print "<p>Hello World!</p>"
---
+++ webserver.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# - *- coding: utf- 8 - *-
+
+import BaseHTTPServer
+import CGIHTTPServer
+import cgitb
+cgitb.enable()
+
+server = BaseHTTPServer.HTTPServer
+handler = CGIHTTPServer.CGIHTTPRequestHandler
+server_address = ("", 8000)
+handler.cgi_directories = ["/cgi-bin"]
+
+httpd = server(server_address, handler)
+httpd.serve_forever()
